d2b94ad9bfc8f93e886821eb75f5a2952be2f955
xAldox995 <valzanialdo@gmail.com>
2024-11-11 18:29:07 +0100

    Adds and test CRUD of Provincia

--- src/main/java/gruppo4/ALDAPAMA/controllers/ProvinciaController.java
+++ src/main/java/gruppo4/ALDAPAMA/controllers/ProvinciaController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.naming.Binding;
 import java.util.stream.Collectors;
 
 /*
@@ -35,5 +37,32 @@ public class ProvinciaController {
     }
 
     @GetMapping
-    public Page<Provincia> findAllProvince(@RequestParam)
+    public Page<Provincia> findAllProvince(@RequestParam(defaultValue = "0") int page,
+                                           @RequestParam(defaultValue = "10") int size) {
+        return this.provinciaServ.findAllProvince(page, size);
+    }
+
+    @GetMapping("/{id_provincia}")
+    public Provincia findProvinciaById(@PathVariable long id_provincia) {
+        return this.provinciaServ.findProvinciaById(id_provincia);
+    }
+
+    @PutMapping("/{id_provincia}")
+    public Provincia findProvinciaByIdAndUp(@PathVariable long id_provincia,
+                                            @RequestBody @Validated ProvinciaDTO body,
+                                            BindingResult validationResult) {
+        if (validationResult.hasErrors()){
+            String msg = validationResult.getAllErrors().stream().map(objectError ->
+                    objectError.getDefaultMessage()).collect(Collectors.joining(", "));
+            throw new BadRequestException("Ci sono stati errori nel payload! "+ msg);
+        }
+        return this.provinciaServ.findProvinciaByIdAndUp(id_provincia,body);
+    }
+
+    @DeleteMapping("/{id_provincia}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void findProvinciaByIdAndDel(@PathVariable long id_provincia){
+        this.provinciaServ.findProvinciaByIdAndDel(id_provincia);
+    }
+
 }
--- src/main/java/gruppo4/ALDAPAMA/services/ProvinciaServ.java
+++ src/main/java/gruppo4/ALDAPAMA/services/ProvinciaServ.java
@@ -3,6 +3,7 @@ package gruppo4.ALDAPAMA.services;
 import gruppo4.ALDAPAMA.dto.ProvinciaDTO;
 import gruppo4.ALDAPAMA.entities.Provincia;
 import gruppo4.ALDAPAMA.exceptions.BadRequestException;
+import gruppo4.ALDAPAMA.exceptions.NotFoundException;
 import gruppo4.ALDAPAMA.repositories.ProvinciaRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,19 +17,43 @@ public class ProvinciaServ {
     private ProvinciaRepo provinciaRepo;
 
     public Provincia saveProvincia(ProvinciaDTO body) {
+        this.checkNomeOrSigla(body);
+        Provincia newProvincia = new Provincia(body.nome(), body.sigla());
+        return this.provinciaRepo.save(newProvincia);
+    }
+
+    private void checkNomeOrSigla(ProvinciaDTO body) {
         this.provinciaRepo.findByNomeOrSigla(body.nome(), body.sigla())
                 .ifPresent(
                         provincia -> {
                             throw new BadRequestException("Provincia " + body.nome() + " è già esistente");
                         }
                 );
-        Provincia newProvincia = new Provincia(body.nome(), body.sigla());
-        return this.provinciaRepo.save(newProvincia);
     }
 
     public Page<Provincia> findAllProvince(int page, int size) {
         if (size > 10) size = 10;
-        Pageable pageable= PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size);
         return this.provinciaRepo.findAll(pageable);
+    }
+
+    public Provincia findProvinciaById(long id_provincia) {
+        return this.provinciaRepo.findById(id_provincia).orElseThrow(() ->
+                new NotFoundException("Provoncia con questo id non trovato"));
+    }
+
+    public Provincia findProvinciaByIdAndUp(long id_provincia, ProvinciaDTO body) {
+        Provincia provFound = this.findProvinciaById(id_provincia);
+        if (!provFound.getNome().equals(body.nome()) || !provFound.getSigla().equals(body.sigla())) {
+            this.checkNomeOrSigla(body);
+        }
+        provFound.setNome(body.nome());
+        provFound.setSigla(body.sigla());
+        return this.provinciaRepo.save(provFound);
+    }
+
+    public void findProvinciaByIdAndDel(long id_provincia) {
+        Provincia provToDel = this.findProvinciaById(id_provincia);
+        this.provinciaRepo.delete(provToDel);
     }
 }
