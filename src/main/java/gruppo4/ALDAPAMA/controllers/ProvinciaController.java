31da9ba9c81867580603bf21cdab89fb83da7c01
xAldox995 <valzanialdo@gmail.com>
2024-11-11 18:47:36 +0100

    fixs

--- src/main/java/gruppo4/ALDAPAMA/controllers/ProvinciaController.java
+++ src/main/java/gruppo4/ALDAPAMA/controllers/ProvinciaController.java
@@ -11,7 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.naming.Binding;
 import java.util.stream.Collectors;
 
 /*
