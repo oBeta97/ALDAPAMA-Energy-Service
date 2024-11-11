c5a581723b66a8a46a3588819648bacac4f0036e
oBeta97 <paolopellizzari97@gmail.com>
2024-11-11 17:23:21 +0100

    not found exception

--- src/main/java/gruppo4/ALDAPAMA/services/UtentiService.java
+++ src/main/java/gruppo4/ALDAPAMA/services/UtentiService.java
@@ -2,6 +2,7 @@ package gruppo4.ALDAPAMA.services;
 
 
 import gruppo4.ALDAPAMA.entities.Utente;
+import gruppo4.ALDAPAMA.exceptions.NotFoundException;
 import gruppo4.ALDAPAMA.repositories.UtentiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -17,11 +18,16 @@ public class UtentiService {
     private UtentiRepository utentiRepo;
 
     public Page<Utente> getAll(int page, int size, String sortBy){
-
         if (size > 25) size = 25;
 
         Pageable pageable = PageRequest.of(page,size, Sort.by(sortBy));
         return this.utentiRepo.findAll(pageable);
-
     }
+
+    public Utente getById (long userId){
+        return this.utentiRepo.findById(userId).orElseThrow(() -> new NotFoundException("Utente con id " + userId + " non trovato!"));
+    }
+
+//    public Utente saveNewUtente (){}
+
 }
