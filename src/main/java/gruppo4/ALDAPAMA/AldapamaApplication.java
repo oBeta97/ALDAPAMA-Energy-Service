3d37895ad1de2333c9c8f8f1f99ddc910a2c8e79
oBeta97 <paolopellizzari97@gmail.com>
2024-11-11 13:44:08 +0100

    init progetto

---
+++ src/main/java/gruppo4/ALDAPAMA/AldapamaApplication.java
@@ -0,0 +1,13 @@
+package gruppo4.ALDAPAMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldapamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AldapamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gruppo4/ALDAPAMA/AldapamaApplicationTests.java
@@ -0,0 +1,13 @@
+package gruppo4.ALDAPAMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AldapamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
