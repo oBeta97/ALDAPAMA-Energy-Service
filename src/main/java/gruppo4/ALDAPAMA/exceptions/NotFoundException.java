5fe1b6a0c38b20c9cb967d9487e41c59c12ee149
Davide Malavenda <davide.malavenda26@gmail.com>
2024-11-11 19:20:29 +0100

    created crud clienti

--- src/main/java/gruppo4/ALDAPAMA/exceptions/NotFoundException.java
+++ src/main/java/gruppo4/ALDAPAMA/exceptions/NotFoundException.java
@@ -1,7 +1,7 @@
 package gruppo4.ALDAPAMA.exceptions;
 
 public class NotFoundException extends RuntimeException {
-    public <T> NotFoundException(Class<T> entity) {
-        super(entity.getSimpleName() + " not found!");
+    public NotFoundException(String message) {
+        super(message);
     }
 }
---
+++ src/main/java/gruppo4/ALDAPAMA/services/ClienteService.java
@@ -0,0 +1,59 @@
+package gruppo4.ALDAPAMA.services;
+
+import gruppo4.ALDAPAMA.dto.ClienteDTO;
+import gruppo4.ALDAPAMA.entities.Cliente;
+import gruppo4.ALDAPAMA.exceptions.BadRequestException;
+import gruppo4.ALDAPAMA.exceptions.NotFoundException;
+import gruppo4.ALDAPAMA.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Page<Cliente> getAll(int page, int size, String sortBy) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return clienteRepository.findAll(pageable);
+    }
+
+    public Cliente getById(Long id) {
+        return clienteRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Cliente con id: " + id + " non trovato"));
+    }
+
+    public Cliente create(ClienteDTO clienteDTO) {
+        if (clienteRepository.existsByPartitaIva(clienteDTO.partitaIva())) {
+            throw new BadRequestException("Partita IVA già esistente");
+        }
+
+        Cliente cliente = new Cliente(
+            clienteDTO.ragioneSociale(),
+            clienteDTO.partitaIva(),
+            clienteDTO.pec(),
+            clienteDTO.telefono(),
+            clienteDTO.logoAziendale()
+        );
+
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente update(Long id, ClienteDTO clienteDTO) {
+        Cliente esistente = getById(id);
+        Cliente clienteAggiornato = new Cliente(id, clienteDTO.ragioneSociale(), 
+            clienteDTO.pec(), clienteDTO.telefono(), clienteDTO.dataUltimoContatto(), 
+            clienteDTO.logoAziendale(), esistente.getDataInserimento());
+        return clienteRepository.save(clienteAggiornato);
+    }
+
+    public void delete(Long id) {
+        Cliente cliente = getById(id);
+        clienteRepository.delete(cliente);
+    }
+}
