3e003c2844c2a69b48d347c41cb1ae36994fe0bb
xAldox995 <valzanialdo@gmail.com>
2024-11-11 16:43:54 +0100

    adds some exceptions

---
+++ src/main/java/gruppo4/ALDAPAMA/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package gruppo4.ALDAPAMA.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
