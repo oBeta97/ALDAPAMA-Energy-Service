e6fd793b2c351144d7a7e09d23b02e9839a1af5f
xAldox995 <valzanialdo@gmail.com>
2024-11-12 12:30:56 +0100

    adds put comune but test failed

--- src/main/java/gruppo4/ALDAPAMA/controllers/ComuneController.java
+++ src/main/java/gruppo4/ALDAPAMA/controllers/ComuneController.java
@@ -1,6 +1,7 @@
 package gruppo4.ALDAPAMA.controllers;
 
 import gruppo4.ALDAPAMA.dto.ComuneDTO;
+import gruppo4.ALDAPAMA.dto.ProvinciaDTO;
 import gruppo4.ALDAPAMA.entities.Comune;
 import gruppo4.ALDAPAMA.exceptions.BadRequestException;
 import gruppo4.ALDAPAMA.services.ComuneServ;
@@ -25,7 +26,7 @@ public class ComuneController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Comune saveComune(@RequestBody @Validated ComuneDTO body,
-                                   BindingResult validationResult) {
+                             BindingResult validationResult) {
         if (validationResult.hasErrors()) {
             String msg = validationResult.getAllErrors().stream()
                     .map(objectError -> objectError.getDefaultMessage())
@@ -34,9 +35,27 @@ public class ComuneController {
         }
         return this.comuneServ.saveComune(body);
     }
+
     @GetMapping
     public Page<Comune> findAllComuni(@RequestParam(defaultValue = "0") int page,
-                                      @RequestParam(defaultValue = "30")int size){
+                                      @RequestParam(defaultValue = "30") int size) {
         return this.comuneServ.findAllComuni(page, size);
+    }
+
+    @GetMapping("/{id_comune}")
+    public Comune findComuneById(@PathVariable long id_comune) {
+        return this.comuneServ.findComuneById(id_comune);
+    }
+
+    @PutMapping("/{id_comune}")
+    public Comune findComuneByIdAndUp(@PathVariable long id_comune,
+                                      @RequestBody @Validated ComuneDTO body,
+                                      BindingResult validationResult){
+        if (validationResult.hasErrors()){
+            String msg = validationResult.getAllErrors().stream().map(objectError ->
+                    objectError.getDefaultMessage()).collect(Collectors.joining(", "));
+            throw new BadRequestException("Ci sono stati errori nel payload! "+ msg);
+        }
+        return this.comuneServ.findComuneByIdAndUp(id_comune,body);
     }
 }
--- src/main/java/gruppo4/ALDAPAMA/services/ComuneServ.java
+++ src/main/java/gruppo4/ALDAPAMA/services/ComuneServ.java
@@ -5,12 +5,15 @@ import gruppo4.ALDAPAMA.dto.ProvinciaDTO;
 import gruppo4.ALDAPAMA.entities.Comune;
 import gruppo4.ALDAPAMA.entities.Provincia;
 import gruppo4.ALDAPAMA.exceptions.BadRequestException;
+import gruppo4.ALDAPAMA.exceptions.NotFoundException;
 import gruppo4.ALDAPAMA.repositories.ComuneRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import javax.swing.plaf.PanelUI;
 
 @Service
 public class ComuneServ {
@@ -36,10 +39,26 @@ public class ComuneServ {
         return this.comuneRepo.save(newComune);
     }
 
-    public Page<Comune> findAllComuni(int page,int size){
-        if (size>30) size= 30;
+    public Page<Comune> findAllComuni(int page, int size) {
+        if (size > 30) size = 30;
         Pageable pageable = PageRequest.of(page, size);
         return this.comuneRepo.findAll(pageable);
     }
+
+    public Comune findComuneById(long id_comune) {
+        return this.comuneRepo.findById(id_comune).orElseThrow(() ->
+                new NotFoundException("Comune con questo id non trovato"));
+    }
+
+    public Comune findComuneByIdAndUp(long id_comune, ComuneDTO body) {
+        Comune comuneFound = this.findComuneById(id_comune);
+        if (!comuneFound.getNome().equals(body.nome()) || comuneFound.getProvincia().getId() != body.id_provincia()) {
+            this.checkNomeAndIdProvincia(body);
+        }
+        comuneFound.setNome(body.nome());
+        comuneFound.setProvincia(this.provinciaServ.findProvinciaById(body.id_provincia()));
+        return this.comuneRepo.save(comuneFound);
+    }
+
 
 }
